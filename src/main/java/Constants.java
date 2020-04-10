c8f24e6b4a20274377a3a63a2e7680cc1cc72920
Alexander Petrushin <stuartrevol@gmail.com>
2020-04-10 16:30:37 +0300

    Created constants interface

---
+++ src/main/java/Constants.java
@@ -0,0 +1,9 @@
+public interface Constants {
+  String[] excludedFolders = {
+    "node_modules", ".vscode", ".idea", "e2e", ".gitlab", "k8s"
+  };
+  String[] excludedFiles = {
+    "tslint.json", "package.json", "package-lock.json",
+    "package.json", "tsconfig", "angular.json"
+  };
+}
